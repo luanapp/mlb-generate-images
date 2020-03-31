ee4cffee7089fc848ecf2a1260bfb5b3357bbd3e
Luana Pimentel <luana.pimentel85@gmail.com>
2020-03-30 23:21:57 -0300

    Added error handling to response

--- src/handler.js
+++ src/handler.js
@@ -6,26 +6,39 @@ const { plants, stands } = require('./components/positions');
 const { BASE_FILENAME, BASE_FILE_FOLDER, PLANTS_FOLDER, STANDS_FOLDER } = process.env;
 
 const generate = async event => {
-  const percentage = pathOr(100, ['queryStringParameters', 'percentage'], event);
-  const baseFile = getFileGlobalUrl(BASE_FILE_FOLDER, BASE_FILENAME);
+  try {
+    const percentage = pathOr(100, ['queryStringParameters', 'percentage'], event);
+    const baseFile = getFileGlobalUrl(BASE_FILE_FOLDER, BASE_FILENAME);
 
-  const standsBuffer = await generateBuffer({ percentage, imageFolder: STANDS_FOLDER, positions: stands, baseFile });
-  const compositeBuffer = await generateBuffer({
-    percentage,
-    imageFolder: PLANTS_FOLDER,
-    positions: plants,
-    baseFile: standsBuffer,
-  });
+    const standsBuffer = await generateBuffer({ percentage, imageFolder: STANDS_FOLDER, positions: stands, baseFile });
+    const compositeBuffer = await generateBuffer({
+      percentage,
+      imageFolder: PLANTS_FOLDER,
+      positions: plants,
+      baseFile: standsBuffer,
+    });
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'image/png',
-      'Content-Disposition': 'attachment; filename="garden.png"',
-    },
-    body: compositeBuffer.toString('base64'),
-    isBase64Encoded: true,
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'image/png',
+        'Content-Disposition': 'attachment; filename="garden.png"',
+      },
+      body: compositeBuffer.toString('base64'),
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        status: 500,
+        message: error,
+      }),
+    };
+  }
 };
 
 const generateBuffer = async ({ percentage, imageFolder, positions: { POSITION_MAP, POSITION_ORDER }, baseFile }) => {
